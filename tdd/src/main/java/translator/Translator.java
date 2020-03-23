0290211748fba57dccff252aec9b08aaaf515732
Arthur freitas <abf2@cin.ufpe.br>
2020-03-22 23:49:52 -0300

    Refactor methods args to be idiom independent

--- tdd/src/main/java/translator/Translator.java
+++ tdd/src/main/java/translator/Translator.java
@@ -15,13 +15,13 @@ public class Translator {
 		return !this.translations.isEmpty();
 	}
 
-	public void addTranslation(String portugueseWord, String englishTranslation) {
-		if(this.translations.containsKey(portugueseWord))
-			englishTranslation = this.translations.get(portugueseWord) + ", " + englishTranslation; 
-		this.translations.put(portugueseWord, englishTranslation);
+	public void addTranslation(String word, String translation) {
+		if(this.translations.containsKey(word))
+			translation = this.translations.get(word) + ", " + translation; 
+		this.translations.put(word, translation);
 	}
 
-	public String translate(String portugueseWord) {
-		return this.translations.get(portugueseWord);
+	public String translate(String word) {
+		return this.translations.get(word);
 	}
 }
